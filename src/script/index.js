c721d4383b53a25ca679fa70193ab2019f891ef1
Folym <heitor.folim18@gmail.com>
2025-10-12 23:01:50 -0300

    Criação do index e configuração das rotas.

---
+++ src/script/index.js
@@ -0,0 +1,8 @@
+
+document.getElementById("btnHost").addEventListener("click", function(){
+    window.location.href = "./src/pages/host.html"
+})
+
+document.getElementById("btnPlayer").addEventListener("click", function(){
+    window.location.href = "./src/pages/player.html"
+})
